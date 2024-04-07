64d6c2b24fb451e22722f287d478f3aa5ab29966
Josusp0 <163787382+Josusp0@users.noreply.github.com>
2024-04-07 16:01:35 -0600

    Add files via upload

---
+++ tarea 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int num = 1;
+	int sum = 0;
+	printf("Secuencia y suma de números pares\n");
+	while (num <= 5) {
+		printf("%d\n", num);
+		if (num % 2 == 0) {
+			sum += num;
+		}
+		num++;
+	}
+	printf(" suma numeros pares: %d\n", sum);
+	return 0;
+}
