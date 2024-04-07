39dbd2434bf24a29b96949fc98c93f61e8c0d2a5
Josusp0 <163787382+Josusp0@users.noreply.github.com>
2024-04-07 16:05:40 -0600

    Add files via upload

---
+++ Tarea 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int num;
+	printf("Introduce un número para la tabla de multiplicar.: ");
+	scanf("%d", &num);
+	printf("tabla de multiplicar de %d:\n", num);
+	for (int i = 1; i <= 10; i++) {
+		printf("%d x %d = %d\n", num, i, num * i);
+	}
+	return 0;
+}
