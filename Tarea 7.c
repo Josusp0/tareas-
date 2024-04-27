2f72d136f9cb2757478de80ada1d67340dbcdf6f
Josusp0 <163787382+Josusp0@users.noreply.github.com>
2024-04-26 22:59:38 -0600

    Add files via upload

---
+++ Tarea 7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+		int num, i, suma_impares = 0;
+		
+		printf("Por favor, ingrese un número: ");
+		scanf("%d", &num);
+		
+		for (i = 1; i <= num; i++) {
+			printf("%d ", i);
+			if (i % 2 != 0) {
+				suma_impares += i;
+			}
+		}
+		
+		printf("\nLa suma de los números impares es: %d\n", suma_impares);
+		
+		return 0;
+	}
+
+
